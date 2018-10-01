fccecb782ff2cd6f7b6726529f37fadde7b82216
Ben <moon791017@gmail.com>
2018-10-01 10:43:21 +0800

    OK

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-var recursive = require("recursive-readdir");
+const recursive = require("recursive-readdir");
 const fs = require('fs');
+const babel = require("@babel/core")
+const returnIgnoreFiles = () => ["*.compiled", "*.xlsx", "*.diagram", "*.svg",
+    "*.css", "*.pdb", "*.mp3", "*.ogg", "*.vbhtml", "*.xml", "*.cs",
+    "*.dll", "*.jpg", "*.png", "*.gif", "*.t4", "*.datasource", "*.config", "*.json",
+    "*.php", "*.ico", "*.map", "*.resx", "*.dwg", "*.eot", "*.woff", "*.ttf", "*.eot", "*.otf", "*.min.js", "*.txt", "LICENSE", "*.ashx"];
 
-const returnIgnoreFiles=()=> ["*.compiled","*.xlsx","*.diagram","*.svg",
-"*.css","*.pdb","*.mp3","*.ogg","*.vbhtml","*.xml","*.cs",
-"*.dll","*.jpg","*.png","*.gif","*.t4","*.datasource","*.config","*.json",
-"*.php","*.ico","*.map","*.resx","*.dwg","*.eot","*.woff","*.ttf","*.eot","*.otf","*.min.js","*.txt","LICENSE","*.ashx"];
-
-const getFileRecursive=()=>  new Promise((resolve, reject) => {
+const getFileRecursive = () => new Promise((resolve, reject) => {
     recursive("D://MDM", returnIgnoreFiles()).then(
-    function(files) {
-      console.log("files are", files);
-      resolve(files);
-    },
-    function(error) {
-        resolve(error);
-    }
-  );
+        function (files) {
+            // console.log("files are", files);
+            resolve(files);
+        },
+        function (error) {
+            resolve(error);
+        }
+    );
 });
 getFileRecursive()
-.then(res=>{
-      fs.writeFileSync('abc.json', JSON.stringify(res)  );  
-})
+    .then(res => {
+        res.map(d => {
+            console.log(d);
+            babel.transformFileAsync(d, {
+                presets: [
+                    [
+                        "@babel/preset-env",
+                        {
+
+                            // for uglifyjs...
+                            forceAllTransforms: true,
+                        },
+                    ],
+                ],
+            })
+                .then(result => {
+                    fs.writeFileSync(d, result.code);
+                });
+        })
+    })
